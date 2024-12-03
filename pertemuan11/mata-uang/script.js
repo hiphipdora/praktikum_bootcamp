2a61f46e40834d0f0097f80368a30f64202530ea
hiphipdora <faaay.adistiii@gmail.com>
2024-12-03 11:42:35 +0700

    menambahkan js convert

--- pertemuan11/mata-uang/script.js
+++ pertemuan11/mata-uang/script.js
@@ -1,4 +1,18 @@
 function convert() {
     let v1 = document.getElementById('valuta1').value;
-    document.getElementById('valuta2').value = v1;
+    let s1 = document.getElementById('nilaiTukar').value;
+
+    let hasil; 
+
+    if (s1 == 'toIDR') {
+        hasil = v1 * 15945;
+    }
+    else if (s1 == 'toUSD') {
+        hasil = v1 / 15945;
+    }
+    else {
+        hasil = 'Tidak valid'
+    }
+
+    document.getElementById('valuta2').value = hasil;
 }
